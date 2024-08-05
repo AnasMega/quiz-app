0c4fd2ab3c5a221c1d3b9be8106d7e1a085c57a0
Arham <arhamali63@gmail.com>
2024-08-05 19:02:46 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,151 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import Questions from "../question.json";
 
+type questions = {
+  Question: string;
+  incorrect_answers: string[];
+  category: string;
+  type: string;
+  difficulty: string;
+  correct_answer: string;
+};
 export default function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showRemark, setShowRemark] = useState(false);
+  const [currentStatus, setcurrentStatus] = useState(false);
+  const [answered, setAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+
+  // console.log("Questions", Questions);
+  const handleSubmit = (answer: string) => {
+    const result = allQuestions[0][currentQuestion].correct_answer;
+    if (result == answer) {
+      nextQuestion(true);
+      setScore(score + 1);
+      setcurrentStatus(true);
+    } else {
+      nextQuestion(false);
+      setcurrentStatus(false);
+    }
+    setAnswered(!answered);
+    // console.log("real asnwer", result);
+    // console.log("User Answer", answer);
+  };
+  const nextQuestion = (stats: boolean) => {
+    if (stats) {
+      setShowRemark(true);
+    } else {
+      // setCurrentQuestion(currentQuestion + 1);
+      setShowRemark(true);
+    }
+  };
+  const marks = (score / 20) * 100;
+
+  console.log("marks", marks);
+  const temp = new Array();
+  const allQuestions = Questions.map((item) => {
+    temp.push({
+      Question: decodeURIComponent(item.question),
+      incorrect_answers: fisherYatesShuffle([
+        ...item.incorrect_answers,
+        item.correct_answer,
+      ]),
+      category: decodeURIComponent(item.category),
+      type: item.type,
+      difficulty: item.difficulty,
+      correct_answer: decodeURIComponent(item.correct_answer),
+    });
+    return temp;
+    //  console.log("item",item)
+  });
+
+  const handleNextQuestion = () => {
+    setAnswered(false);
+    if (currentStatus) {
+      setCurrentQuestion(currentQuestion + 1);
+      setShowRemark(false);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      setShowRemark(false);
+    }
+  };
+  function fisherYatesShuffle(arr: any) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+  console.log("Score", score);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <div className=" w-full bg-white flex flex-col ">
+        <span
+          style={{ width: score * 60 }}
+          className={`   h-8 bg-green-400`}
+        ></span>
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className=" flex flex-col">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b  text-lg bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Question {currentQuestion + 1} out of {Questions?.length}&nbsp;
+          </p>
+
+          <p className=" text-sm flex justify-center">
+            {decodeURIComponent(Questions[currentQuestion].category)} Board Game
+          </p>
+          <p className=" text-sm flex justify-start  pl-8">
+            {Questions[currentQuestion].difficulty == "hard"
+              ? "⭐⭐⭐⭐⭐"
+              : Questions[currentQuestion].difficulty == "medium"
+              ? "⭐⭐⭐"
+              : "⭐"}
+          </p>
         </div>
       </div>
+      <div>{decodeURIComponent(Questions[currentQuestion].question)}</div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        {showRemark && (
+          <div className=" flex flex-col space-y-4">
+            <p className=" justify-center flex  tex-lg font-bold ">
+              {currentStatus ? "Correct!" : "Wrong!"}
+            </p>
+            <button
+              onClick={() => handleNextQuestion()}
+              className="bg-gray-400 p-2 rounded-sm text-black font-semibold"
+            >
+              Next Question
+            </button>
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className=" flex  space-x-12">
+        {allQuestions[0][currentQuestion].incorrect_answers.map(
+          (data: string) => (
+            <button
+              disabled={answered}
+              className=" bg-gray-400 rounded-sm p-3 hover:bg-slate-900  hover:text-white text-black"
+              onClick={() => handleSubmit(decodeURIComponent(data))}
+            >
+              {decodeURIComponent(data)}
+            </button>
+          )
+        )}
+      </div>
+      <p>{"Score " + marks + "%"}</p>
+      <div className=" w-full bg-white flex flex-row ">
+        <span
+          style={{ width: marks * 10 }}
+          className={`   h-8 ${
+            marks < 30
+              ? "bg-red-400"
+              : marks > 30 && marks < 50
+              ? "bg-yellow-400"
+              : "bg-green-400"
+          } `}
+        ></span>
       </div>
     </main>
   );
